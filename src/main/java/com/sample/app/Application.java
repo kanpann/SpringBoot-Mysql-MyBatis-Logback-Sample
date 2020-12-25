6202469b58257eee9336760f48f0f72fb5df7f5b
gunkim0318 <gunkim0318@gmail.com>
2020-12-25 09:06:31 +0900

    mysql 셋팅

--- src/main/java/com/sample/app/Application.java
+++ src/main/java/com/sample/app/Application.java
@@ -1,13 +1,11 @@
-package com.sample.sample;
+package com.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
--- src/test/java/com/sample/app/ApplicationTests.java
+++ src/test/java/com/sample/app/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sample.sample;
+package com.sample.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SampleApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
